55e258bfb51c16e421925869be58881051df41e7
Pranav Pise <itspranavpise@gmail.com>
2025-01-29 16:29:04 +0530

    update: change the pipeline sequence

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 const MovieCard = ({ movie:
   { title, vote_average, poster_path, release_date, original_language }
@@ -7,7 +7,7 @@ const MovieCard = ({ movie:
     <div className="movie-card">
       <img
         src={poster_path ?
-          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : "/no-movie.png"}
         alt={title}
       />
 
@@ -17,7 +17,7 @@ const MovieCard = ({ movie:
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
 
           <span>•</span>
@@ -25,7 +25,7 @@ const MovieCard = ({ movie:
 
           <span>•</span>
           <p className="year">
-            {release_date ? release_date.split('-')[0] : 'N/A'}
+            {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
         </div>
       </div>
